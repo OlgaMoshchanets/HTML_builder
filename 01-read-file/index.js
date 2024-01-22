d5e9ab2189c8228d0f2db2e2d089a999830f2d0a
Olga Moshchanets <o.moshchanets@gmail.com>
2024-01-22 21:13:53 +0400

    feat: add 01 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'),'utf-8');
+stream.on('data', (data) => {
+  stdout.write(data);
+});
+
